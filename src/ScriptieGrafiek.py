c876fe5db5ada7edfa88cc7740d3d05f85f8f029
Pim Spelier <spelier.pim@gmail.com>
2015-11-29 11:09:49 +0100

    Added graph function

--- src/ScriptieGrafiek.py
+++ src/ScriptieGrafiek.py
@@ -33,6 +33,121 @@ def smallest_solution_Pell(d):
         #print(a,b,k,a**2-d*b**2)
     return [a,b]
 
+def graph(n):
+    import turtle
+    import math
+
+    grapher = turtle.Turtle()
+    window = turtle.Screen()
+    window.screensize(1100,600)
+    window.title("Grafiek van de regulator")
+
+    turtle.setup(1150, 650)
+
+    maxy = n**0.5*math.log(n)
+
+    grapher.ht()
+    grapher.speed(0)
+    grapher.pu()
+    grapher.goto(-500,275)
+
+    def draw_right1(some_turtle, to_write):
+        some_turtle.right(90)
+        some_turtle.fd(5)
+
+        some_turtle.pu()
+        
+        some_turtle.left(90)
+        some_turtle.fd(5)
+        some_turtle.write(to_write, align = "right")
+        some_turtle.bk(5)
+        some_turtle.right(90)
+
+        some_turtle.pd()
+        
+        some_turtle.bk(5)
+        some_turtle.left(90)
+
+    grapher.pd()
+    grapher.seth(270)
+
+    grapher.fd(25)
+
+    draw_right1(grapher,int(maxy+0.5))
+
+    for i in range(4):
+        grapher.fd(125)
+        draw_right1(grapher,int(maxy*(3-i)/4+0.5))
+        
+    grapher.left(90)
+
+    def draw_right2(some_turtle, to_write):
+        some_turtle.right(90)
+        some_turtle.fd(5)
+
+        some_turtle.pu()
+        
+        #some_turtle.left(90)
+        some_turtle.fd(15)
+        some_turtle.write(to_write, align = "center")
+        some_turtle.bk(15)
+        #some_turtle.right(90)
+
+        some_turtle.pd()
+        
+        some_turtle.bk(5)
+        some_turtle.left(90)
+
+    draw_right2(grapher,0)
+
+    for i in range(4):
+        grapher.fd(250)
+        draw_right2(grapher,(i+1)*n//4)
+
+    grapher.fd(25)
+
+
+    grapher.pu()
+    grapher.goto(-500,-250)
+    grapher.pd()
+    grapher.pencolor("red")
+
+    for i in range(1,n+1):
+        fi = i**0.5*math.log(i) + 1
+        #print(i,fi)
+        grapher.goto((i/n)*1000-500,(fi/maxy)*500-250)
+
+    grapher.pu()
+    #grapher.goto(520-500,450-250)
+    #grapher.write("Complexiteit bovengrens van R_d: ln(d)d^0.5",align="left", font=("Arial", 8, "normal"))
+
+
+    grapher.goto(-500,-250)
+    grapher.pd()
+    grapher.pencolor("blue")
+
+    for i in range(1,n+1):
+        fi = math.log(2*i**0.5)
+        #print(i,fi)
+        grapher.goto((i/n)*1000-500,(fi/maxy)*500-250)
+
+    grapher.pu()
+    #grapher.goto(520-500,-20-250)
+    #grapher.write("Complexiteit ondergrens van R_d: ln(2*d^0.5)",align="left", font=("Arial", 8, "normal"))
+
+    grapher.pencolor("green")
+
+    for i in range(1,n+1):
+        if int(i**0.5+0.5)**2==i:
+            continue
+        [a,b] = smallest_solution_Pell(i)
+        R_d = math.log(a+b*i**0.5)
+        grapher.goto((i/n)*1000-500,(R_d/maxy)*500-250)
+        grapher.dot(3,"green")
+
+    #grapher.goto(520-500,200-250)
+    #grapher.write("R_d",align="left", font=("Arial", 8, "normal"))
+
 class ZfZsqrtd():
     def __init__(self,a,b,d,f=-1):
         if f==-1:
